346d285dee1ca9d798d2167475e3f78ee32f4a7b
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-03-16 16:59:47 -0300

    Gabarito Lista2, Marca

--- objetivo1_lista2/model/Marca_Lista2.java
+++ objetivo1_lista2/model/Marca_Lista2.java
@@ -1,15 +1,25 @@
 package model;
 
 public class Marca_Lista2 {
+	private int id;
 	private String descricao;
 
 	public Marca_Lista2() {
 		super();
 	}
 
-	public Marca_Lista2(String descricao) {
+	public Marca_Lista2(int id, String descricao) {
 		super();
+		this.id = id;
 		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getDescricao() {
@@ -22,7 +32,7 @@ public class Marca_Lista2 {
 
 	@Override
 	public String toString() {
-		return "Marca [descricao=" + descricao + "]";
+		return "\nMarca_Lista2 [id=" + id + ", descricao=" + descricao + "]";
 	}
 	
 }
