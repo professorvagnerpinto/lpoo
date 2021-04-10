c0ccc63c7be2baca8fcc50da0ff24db7606c3b63
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-04-10 16:55:05 -0300

    Herança Simples e Herança Múltipla

---
+++ objetivo2_heranca_simples_v/model/ContaPoupancaSalario.java
@@ -0,0 +1,10 @@
+package model;
+
+public class ContaPoupancaSalario extends ContaPoupancaV {
+
+	@Override
+	public String toString() {
+		return "\nContaPoupancaSalario [Saldo=" + getSaldo() + "]";
+	}
+	
+}
