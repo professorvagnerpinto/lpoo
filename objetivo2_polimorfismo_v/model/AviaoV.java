e1f61541a83abdcd183e8aba4f8df81d752295e4
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-04-11 15:37:58 -0300

    Polimorfismo, vídeos

--- objetivo2_polimorfismo_v/model/AviaoV.java
+++ objetivo2_polimorfismo_v/model/AviaoV.java
@@ -1,5 +1,37 @@
 package model;
 
-public class AviaoV {
+public class AviaoV extends PersonagemV{
+
+	private double z=0;
+	
+	public AviaoV() {
+		super();
+	}
+
+	public AviaoV(double x, double y) {
+		super(x, y);
+	}
+	
+	public AviaoV(double x, double y, double z) {
+		super(x, y);
+		this.z = z;
+	}
+	
+	public void mover(double x, double y, double z) {
+		super.mover(x, y);
+		this.z = z;
+	}
+
+	@Override
+	public void desenhar() {
+		System.out.println("Desenhou um avião");	
+	}
+
+	@Override
+	public String toString() {
+		return "\n¬AviaoV [z=" + z + ", x=" + x + ", y=" + y + "]";
+	}
+	
+	
 
 }
--- objetivo2_polimorfismo_v/model/PersonagemV.java
+++ objetivo2_polimorfismo_v/model/PersonagemV.java
@@ -1,5 +1,28 @@
 package model;
 
-public class PersonagemV {
+public abstract class PersonagemV {
+	
+	protected double x=0, y=0;
+	
+	public PersonagemV() {
+		super();
+	}
 
+	public PersonagemV(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public void mover(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public abstract void desenhar();
+
+	@Override
+	public String toString() {
+		return "\n¬PersonagemV [x=" + x + ", y=" + y + "]";
+	}
 }
