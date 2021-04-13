35ae18b2e2c4f399aa56e38c8746978ce1bd736f
Vagner Pinto <professor.vagnerpinto@gmail.com>
2021-04-13 20:20:24 -0300

    Tipo prórpio de Exceção

--- objetivo4_tratamento_excecoes_v/control/TratamentoExcecoesV.java
+++ objetivo4_tratamento_excecoes_v/control/TratamentoExcecoesV.java
@@ -5,72 +5,92 @@ import java.util.InputMismatchException;
 public class TratamentoExcecoesV {
 
 	public static void main(String[] args) {
-		
-		//Divisão por Zero
-		//System.out.println(10/0);
-		
-		//NullPointerException (Ponteiro Nulo)
-		//Conta4V conta1 = null;//new Conta4V();
-		//System.out.println(conta1.toString());
-		
-		//analisando a Pilha de exceção
-		comportamento1(); //chamada de método
+
+		// Divisão por Zero
+		// System.out.println(10/0);
+
+		// NullPointerException (Ponteiro Nulo)
+		// Conta4V conta1 = null;//new Conta4V();
+		// System.out.println(conta1.toString());
+
+		// analisando a Pilha de exceção
+		comportamento1(); // chamada de método
 
 	}
-	
+
 	private static void comportamento1() {
-		comportamento2(); //chamada de método
+		comportamento2(); // chamada de método
 	}
-	
-	private static void comportamento2() {
-		//Divisão por Zero
-		//System.out.println(10/0); //lança a exceção
-		//System.out.println("Executou todas as chamadas.");
 
-		//InputMismatchException exception = new InputMismatchException();
-		//throw exception;
-		
-		
+	private static void comportamento2() {
+		// Divisão por Zero
+		// System.out.println(10/0); //lança a exceção
+		// System.out.println("Executou todas as chamadas.");
+
+		// InputMismatchException exception = new InputMismatchException();
+		// throw exception;
+
 //		try {
 //			throwException();
 //		} catch (Exception e) {
-//			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-		
+
+		try {
+			mythrowException();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	//lança uma exceção verificada.
+
+	// Exemplo de Tipo prórpio de Exceção
+	// lança uma exceção verificada de uma classe criada pelo programador
+	private static void mythrowException() throws Exception {
+		try { //try-catch é um único comando, deve vir juntos
+			System.out.println("Tentou executar o bloco try, mas lança a MinhaExcecao");
+			throw new MinhaExcecao(); // gera a exceção
+		} catch (MinhaExcecao e) { // pega a exceção lançada no bloco try
+			e.printStackTrace(); //imprime a stack de exceção
+		} finally { // executa em qualquer caso, seja resultado da execução do try ou do catch
+			System.err.println("Finally executado em mythrowException()");
+		}
+	}
+
+	// lança uma exceção verificada.
 	/*
-	 * Segundo Deitel (2010), todas as classes que são herdadas da classe Exception, mas não direta ou indiretamente da classe 
-	 * RuntimeException, são consideradas exceções verificadas.
+	 * Segundo Deitel (2010), todas as classes que são herdadas da classe Exception,
+	 * mas não direta ou indiretamente da classe RuntimeException, são consideradas
+	 * exceções verificadas.
 	 */
-	private static void throwException() throws Exception{
+	private static void throwException() throws Exception {
 		// throw an exception and immediately catch it
-		try { 
-		     System.out.println("Method throwException");
-		     throw new Exception(); // generate exception
+		try {
+			System.out.println("Executou no bloco try de throwException()");
+			throw new Exception(); // generate exception
+		} catch (Exception exception) { // catch exception thrown in try
+			System.err.println("Exceção tratata no bloco catch de throwException()");
+			throw exception; // rethrow for further processing
+		} finally { // executes regardless of what occurs in try...catch
+			System.err.println("Finally executado em  throwException()");
 		}
-		catch (Exception exception) { // catch exception thrown in try
-		     System.err.println(
-		        "Exception handled in method throwException");
-		     throw exception; // rethrow for further processing
-		}
-		finally { // executes regardless of what occurs in try...catch
-			System.err.println("Finally executed in throwException");
-		}
-	
-	  // code here would not be reached; would cause compilation errors
-	
+
+		// code here would not be reached; would cause compilation errors
+
 	}
-	
+
 }// fim classe TratamentoExcecoesV
 
+class MinhaExcecao extends Exception {
+	public MinhaExcecao() {
+		super("Meu problema ... forcei um na minha classe");
+	}
+}
 
-
+//Classe utilizada para demonstrar o NullPointerException
 class Conta4V {
-	private Double saldo=0.0;
-	
+	private Double saldo = 0.0;
+
 	public Conta4V() {
 		super();
 	}
@@ -92,5 +112,5 @@ class Conta4V {
 	public String toString() {
 		return "\nConta [saldo=" + saldo + "]";
 	}
-	
+
 }
